4bb099c6360ffe3a2021809e232a664d208fe4e8
1091200311 <1091200311@qq.com>
2018-11-16 11:15:22 +0800

    Initial commit

---
+++ src/main/java/com/soft1611/meizu/MeizuApplication.java
@@ -0,0 +1,12 @@
+package com.soft1611.meizu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeizuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeizuApplication.class, args);
+    }
+}
